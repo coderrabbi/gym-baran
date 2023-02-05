6a6e682e246e04059910830cec43983b07aea4d0
coderrabbie <golamrabbi.dv@gmail.com>
2023-02-05 19:13:05 +0600

    hero section done

--- src/Components/HomeScreen/HomeScreen.js
+++ src/Components/HomeScreen/HomeScreen.js
@@ -1,13 +1,60 @@
 import styles from "@/style";
+import Image from "next/image";
 import React from "react";
 import Layout from "../Layout/Layout";
-
+import { MdNavigateNext } from "react-icons/md";
+import { AiFillPlayCircle } from "react-icons/ai";
+import heroImg from "../../assets/hero-img.png";
+import nike from "../../assets/nike.png";
+import adidas from "../../assets/adidas.png";
+import puma from "../../assets/puma.png";
+import reebok from "../../assets/reebok.png";
 const HomeScreen = () => {
+  const brands = [nike, adidas, puma, reebok];
   return (
     <Layout>
-      <div className={`${styles.padding} flex`}>
-        <div className="flex-1">hello</div>
-        <div className="flex-1">picture</div>
+      <div
+        className={`${styles.padding} flex items-center md:flex-row flex-col md:gap-3 gap-10`}
+      >
+        <div className="flex-1 ">
+          <div className="flex flex-col gap-4">
+            <h1 className={`${styles.heading2}`}>
+              Healthy in side <br />{" "}
+              <span className="text-secondary">fresh</span> out side
+            </h1>
+            <p className={`${styles.paragraph}`}>
+              Exercise is a very important need for our body. Health and <br />{" "}
+              fitness will be obtained if you can do regular exercise and <br />{" "}
+              run a healthy routine.
+            </p>
+            <div className="flex md:gap-4  gap-6 ">
+              <button className="flex py-4 font-poppins font-medium  md:text-[16px]  text-[12px] shadow-custom bg-primary rounded-lg text-white md:w-[246px] w-[176px] px-4 h-[40px] md:h-[56px]  items-center justify-between">
+                Get started{" "}
+                <MdNavigateNext className="bg-white text-primary text-2xl rounded-full" />
+              </button>
+              <button className="flex py-4 font-poppins font-medium md:text-[16px]  text-[12px] shadow-custom bg-white rounded-lg text-primary md:w-[174px] w-[125px] px-4 h-[40px] md:h-[56px] items-center justify-between">
+                <AiFillPlayCircle className="bg-white text-primary text-2xl rounded-full" />
+                Learn more
+              </button>
+            </div>
+            <div>
+              <span>Brands:</span>
+              <div className="flex items-center md:gap-11 gap-5 ">
+                {brands.map((brand, i) => (
+                  <Image
+                    src={brand}
+                    className="md:h-[15%] h-[10%] aspect-[3/2] object-contain"
+                    key={i}
+                    alt={brand}
+                  ></Image>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1">
+          <Image src={heroImg} alt="hero img"></Image>
+        </div>
       </div>
     </Layout>
   );
--- src/style.js
+++ src/style.js
@@ -2,8 +2,9 @@ const styles = {
   boxWidth: "xl:max-w-[1280px] w-full",
 
   heading2:
-    "font-SportingGrotesque font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
-  paragraph: "font-poppins font-normal text-black text-[16px] leading-[30.8px]",
+    "font-SportingGrotesque font-semibold md:text-[56px] text-[28px] text-black md:leading-[73px] leading-[39.2px] w-full",
+  paragraph:
+    "font-poppins font-normal text-[#7c7774] text-[16px] leading-[28px]",
 
   flexCenter: "flex justify-center items-center",
   flexStart: "flex justify-center items-start",
